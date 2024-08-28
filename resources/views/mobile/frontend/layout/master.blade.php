f955e99147fb7a79204f02f8479cee7d795a80da
Agung Mustaqim <AgungMustaqim@eskalink.com>
2024-08-28 17:11:52 +0700

    shgdjffgf

--- resources/views/mobile/frontend/layout/master.blade.php
+++ resources/views/mobile/frontend/layout/master.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('mobile/styles/style.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('mobile/fonts/css/fontawesome-all.min.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900|Roboto:300,300i,400,400i,500,500i,700,700i,900,900i&amp;display=swap" rel="stylesheet">
-    <link rel="manifest" href="_manifest.json" data-pwa-version="set_in_manifest_and_pwa_js">
+    <link rel="manifest" href="{{ asset('mobile/_manifest.json') }}" data-pwa-version="set_in_manifest_and_pwa_js">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('mobile/images/logobulat.png') }}">
     <link rel="icon" href="{{ asset('mobile/images/logobulat.png') }}" type="image/png">
 
