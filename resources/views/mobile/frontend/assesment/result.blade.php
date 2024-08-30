05dc8094490b52630e53acd7faea5944174baad0
Agung Mustaqim <AgungMustaqim@eskalink.com>
2024-08-30 08:26:18 +0700

    sghdfghdfgh

--- resources/views/mobile/frontend/assesment/result.blade.php
+++ resources/views/mobile/frontend/assesment/result.blade.php
@@ -52,25 +52,80 @@
                         <td style="padding: 10px; border: none;">{{ number_format($bbIdeal, 2) }} kg</td>
                     </tr>
                     <tr>
-                        <td style="padding: 10px; border: none;"><strong>Kebutuhan Kalori Harian tanpa aktivitas:</strong></td>
+                        <td style="padding: 10px; border: none;"><strong>Kebutuhan Kalori Harian tanpa aktivitas:</strong>
+                        </td>
                         <td style="padding: 10px; border: none;">{{ number_format($bmrBase, 2) }} kcal/hari</td>
                     </tr>
                     <tr>
-                        <td style="padding: 10px; border: none;"><strong>Kebutuhan Kalori Harian dengan aktivitas:</strong></td>
+                        <td style="padding: 10px; border: none;"><strong>Kebutuhan Kalori Harian dengan aktivitas:</strong>
+                        </td>
                         <td style="padding: 10px; border: none;">{{ number_format($bmrCorrected, 2) }} kcal/hari</td>
                     </tr>
                     <tr>
                         <td style="padding: 10px; border: none;"><strong>Total Kebutuhan Kalori:</strong></td>
-                        <td style="padding: 10px; border: none;">{{ number_format($totalCaloriesRounded, 2) }} kcal/hari</td>
+                        <td style="padding: 10px; border: none;">{{ number_format($totalCaloriesRounded, 2) }} kcal/hari
+                        </td>
                     </tr>
                 </table>
 
-                <h2>Saran Konsumsi Kalori</h2>
+                {{-- <h2>Saran Konsumsi Kalori</h2> --}}
                 {{-- <ul>
                     @foreach ($recommendedCalories as $food)
                         <li>{{ $food->nama_makanan }} - {{ $food->kalori_per_gram }} kalori per gram</li>
                     @endforeach
                 </ul> --}}
+            </div>
+        </div>
+
+        <div class="content mt-0">
+            <div class="row">
+                <div class="col-6">
+
+                    <h2>Saran</h2>
+                    <ul>
+                        @if ($result == 'Mengalami BED')
+                            <li>Kurangi konsumsi makanan yang tinggi kalori dan gula.</li>
+                            <li>Fokus pada makanan dengan kandungan serat tinggi dan protein rendah kalori.</li>
+                            <li>Konsultasikan dengan ahli gizi untuk mendapatkan rencana makan yang lebih tepat.</li>
+
+                            <div class="content mt-0">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <a href="#" class="btn btn-full btn-m rounded-s text-uppercase font-900 shadow-xl bg-highlight">
+                                            @if (session('lang') === 'id')
+                                                {{ 'Mulai Pemulihan' }}
+                                            @else
+                                                {{ $translate->translate('Mulai Pemulihan') }}
+                                            @endif
+                                        </a>
+                                    </div>
+                                    <div class="col-6">
+                                        <a href="#"
+                                            class="btn btn-full btn-border btn-m rounded-s text-uppercase font-900 shadow-l border-highlight color-highlight">
+                                            @if (session('lang') === 'id')
+                                                {{ 'Hubungi Ahli' }}
+                                            @else
+                                                {{ $translate->translate('Hubungi Ahli') }}
+                                            @endif
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                        @elseif ($result == 'Beresiko BED')
+                            <li>Perhatikan pola makan dan cobalah untuk tidak makan berlebihan.</li>
+                            <li>Ikuti saran diet seimbang dengan porsi makan yang teratur.</li>
+                            <li>Jika diperlukan, pertimbangkan untuk berbicara dengan seorang konselor.</li>
+                        @else
+                            <li>Teruskan pola makan sehat dan gaya hidup aktif.</li>
+                            <li>Pastikan asupan kalori sesuai dengan kebutuhan tubuh Anda.</li>
+                            <li>Selalu jaga keseimbangan nutrisi dalam makanan sehari-hari.</li>
+                        @endif
+                    </ul>
+                </div>
+
             </div>
         </div>
     </div>
